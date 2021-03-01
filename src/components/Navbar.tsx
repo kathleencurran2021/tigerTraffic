a043e048eb94b98bb5e5ffc22cea470a017065ec
Kathleen <kathleencurran_2021@depauw.edu>
2021-03-01 15:32:10 -0500

    added navbar, homepage

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import { IonNav, IonToolbar } from '@ionic/react';
+import React from 'react';
+
+export const Navbar = () => {
+    return (
+        <IonToolbar>
+            
+        </IonToolbar>
+    )
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import { IonContent, IonPage, IonText } from '@ionic/react';
+import React from 'react';
+
+const HomePage: React.FC = () => {
+	return (
+		<IonPage>
+			<IonContent>
+				<IonText><h1>HomePage</h1></IonText>
+			</IonContent>
+		</IonPage>
+	)
+}
